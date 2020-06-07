32418fc16cbffe8232837511d55c76fcdab7a828
Bennani Mohamed <simobenn96@gmail.com>
2020-06-08 00:29:40 +0200

    Install body-parser / Extract data

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/heroes', (req, res) => {
-    console.log('Bonjour BenTen')
+    console.log(req.body)
 })
 
 app.listen(3000, function () {

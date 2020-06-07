9427c4c05c9278768703b9a2018214255f64aa80
Bennani Mohamed <simobenn96@gmail.com>
2020-06-08 00:15:31 +0200

    start post hero

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/heroes', (req, res) => {
+    console.log('Bonjour BenTen')
+})
 
 app.listen(3000, function () {
     console.log('listening on 3000')
